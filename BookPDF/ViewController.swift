078bb0b2633829fe07ca410d558f960036177369
Horace Ho <horaceh@gmail.com>
2022-10-05 18:09:06 +0800

    View bundled PDF

--- BookPDF/ViewController.swift
+++ BookPDF/ViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import PDFKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let pdfView = PDFView()
+
+        pdfView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(pdfView)
+
+        pdfView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        pdfView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        pdfView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        pdfView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+
+        guard let path = Bundle.main.url(forResource: "WatchOS9", withExtension: "pdf") else { return }
+
+        if let document = PDFDocument(url: path) {
+            pdfView.document = document
+        }
     }
 
-
 }
-
